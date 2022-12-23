def4f017f5b3759d3f4a024e07d3f9b9f44174dc
iolchu <ramil.iolchu@gmail.com>
2022-12-23 19:03:13 +0600

    Create NumbersInteractorTest.kt

---
+++ app/src/test/java/com/example/ranfomfactnumbers/numbers/domain/NumbersInteractorTest.kt
@@ -0,0 +1,86 @@
+package com.example.ranfomfactnumbers.numbers.domain
+
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+
+class NumbersInteractorTest {
+
+    private lateinit var interactor: NumbersInteractor
+    private lateinit var repository: TestNumbersRepository
+
+    @Before
+    fun setup() {
+        repository = TestNumbersRepository()
+        interactor = NumbersInteractor.Base(repository)
+    }
+
+    @Test
+    fun test_init_success() = runBlocking {
+        repository.changeExpectedList(listOf(NumberFact("6", "fact about 6")))
+        val actual = interactor.init()
+        val expected = NumbersResult.Success(listOf(NumberFact("6", "fact about 6")))
+
+        assertEquals(expected, actual)
+        assertEquals(1, repository.allNumbersCalledCount)
+    }
+
+    @Test
+    fun test_fact_about_number_success() = runBlocking {
+        repository.changeExpectedFactOfNumber(NumberFact("7", "expected 7"))
+        val actual = interactor.factAboutNumber("7")
+        val expected = NumbersResult.Success(listOf(NumberFact("7", "expected 7")))
+
+        assertEquals(expected, actual)
+        assertEquals("7", repository.numberFactCalledList[0])
+        assertEquals(1, repository.numberFactCalledList.size)
+    }
+
+    @Test
+    fun test_fact_about_number_error() = runBlocking {
+        repository.changeExpectedFactOfNumber(NumberFact("7", "expected 7"))
+        val actual = interactor.factAboutNumber("7")
+        val expected = NumbersResult.Success(listOf(NumberFact("7", "expected 7")))
+
+        assertEquals(expected, actual)
+        assertEquals("7", repository.numberFactCalledList[0])
+        assertEquals(1, repository.numberFactCalledList.size)
+    }
+
+    private class TestNumbersRepository : NumbersRepository {
+
+        private val allNumbers = mutableListOf<NumberFact>()
+        private var numberFact = NumberFact("", "")
+        private var errorWhileNumberFact = false
+
+        var allNumbersCalledCount = 0
+        val numberFactCalledList = mutableListOf<String>()
+
+        fun changeExpectedList(list: List<NumberFact>) {
+            allNumbers.clear()
+            allNumbers.addAll(list)
+        }
+
+        fun changeExpectedFactOfNumber(numberFact: NumberFact) {
+            this.numberFact = numberFact
+        }
+
+        fun expectingErrorGetFact(error: Boolean) {
+            errorWhileNumberFact = error
+        }
+
+        override fun allNumbers(): List<NumberFact> {
+            allNumbersCalledCount++
+            return allNumbers
+        }
+
+        override fun numberFact(number: String): NumberFact {
+            numberFactCalledList.add(number)
+            if (errorWhileNumberFact) throw NoInternetConnectionException()
+            return numberFact
+        }
+
+    }
+
+}
