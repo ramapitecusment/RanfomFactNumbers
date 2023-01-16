8c4a8b71fa12d48b0b98ece04cb085f2ed74fe92
iolchu <ramil.iolchu@gmail.com>
2023-01-16 09:32:56 +0600

    Update NumbersFragment.kt

--- app/src/main/java/com/example/ranfomfactnumbers/numbers/presentation/NumbersFragment.kt
+++ app/src/main/java/com/example/ranfomfactnumbers/numbers/presentation/NumbersFragment.kt
@@ -3,6 +3,7 @@ package com.example.ranfomfactnumbers.numbers.presentation
 import android.os.Bundle
 import android.view.View
 import androidx.core.widget.addTextChangedListener
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
@@ -16,11 +17,7 @@ class NumbersFragment : Fragment(R.layout.fragment_numbers) {
     private val viewModel: NumbersViewModel by viewModel()
 
     private val clickListener = object : ClickListener {
-
-        override fun click(item: NumberUi) {
-            navigateToDetails()
-        }
-
+        override fun click(item: NumberUi) = navigateToDetails()
     }
 
     private val adapter = NumbersAdapter(clickListener)
@@ -34,22 +31,16 @@ class NumbersFragment : Fragment(R.layout.fragment_numbers) {
     }
 
     private fun bindView() {
+        binding.historyRecyclerView.adapter = adapter
         binding.getFact.setOnClickListener {
             viewModel.fetchNumberFact(binding.editText.text.toString())
         }
-        binding.getRandomFact.setOnClickListener {
-            viewModel.fetchRandomNumberFact()
-        }
-        binding.historyRecyclerView.adapter = adapter
-        binding.editText.addTextChangedListener(afterTextChanged = {
-            viewModel.clearError()
-        })
+        binding.getRandomFact.setOnClickListener { viewModel.fetchRandomNumberFact() }
+        binding.editText.doAfterTextChanged { viewModel.clearError() }
     }
 
     private fun observeViewModel() {
-        viewModel.observeState(this) {
-            it.apply(binding.inputEditText)
-        }
+        viewModel.observeState(this) { it.apply(binding.inputEditText) }
         viewModel.observeList(this, adapter::submitList)
         viewModel.observeProgress(this, binding.progress::setVisibility)
     }
